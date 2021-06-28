0ecdea9bbcf3d053590a40d6859c74201b091549
Mjöllnir <37479575+Equinox4@users.noreply.github.com>
2021-06-28 09:09:19 +0200

    Create perl_linter_nano_wrapper.pl
    cette version fonctionne à peu près, il reste des bugs à résoudre

---
+++ perl_linter_nano_wrapper.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+# Ce commentaire est faux, dans le source on check ": "
+# nano text.c l 2640
+# At the moment we handle the following formats:
+#
+#     filenameorcategory:line:column:message (e.g. splint)
+#     filenameorcategory:line,column:message (e.g. pylint)
+#     filenameorcategory:line:message        (e.g. pyflakes)
+
+my $filename_in = shift;
+
+open(PIPE, "perl -c $filename_in 2>&1 |");
+
+my $message = <PIPE>;
+
+close PIPE;
+
+if ($message =~ m/OK/) {
+    exit 0;
+}
+
+$message =~ /(?<error>.+) at (?<filename>.+) line (?<line_number>\d+).$/;
+my ($filename, $line_number, $error) = ($+{'filename'}, $+{'line_number'}, $+{'error'});
+
+if ($error =~ m/locate/) {
+    $error =~ /(?<inc>.+INC) \(/;
+    $error =  $+{inc};
+}
+
+print join(': ', $filename, $line_number, $error), "\n";
+
+exit 1;
